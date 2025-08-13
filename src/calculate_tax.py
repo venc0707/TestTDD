c99fd4ff6c756363e00e5a802142baaf5bd6a9d6
Rodion Ventslovsky <vents_23@mail.ru>
2025-08-13 22:38:41 +0300

    update func calculate_tax + update tests calculate_tax

--- src/calculate_tax.py
+++ src/calculate_tax.py
@@ -1,14 +1,14 @@
 
 
-def calculate_tax(price, tax_rate):
+def calculate_tax(price: float, tax_rate: float, *, discount: float = 0, round_to: int = 2) -> float:
     if price <= 0:
         raise ValueError('Неверная цена')
     elif 0 < tax_rate <= 100:
-        result = (price * tax_rate / 100) + price
-        return result
+        result = (price * tax_rate / 100) + price - (price * discount / 100)
+        return round(result, round_to)
     else:
         raise ValueError('Неверный налоговый процент')
 
 
 if __name__ == "__main__":
-    print(calculate_tax(100, 20))
+    print(calculate_tax(100, 31.4141, round_to=5))
--- tests/test_calculate_tax.py
+++ tests/test_calculate_tax.py
@@ -5,6 +5,11 @@ import pytest
 def test_calculate_tax():
     assert calculate_tax(100.0, 20.0) == 120.0
 
+def test_calcelate_tax_discount():
+    assert  calculate_tax(100.0, 20.0, discount=10) == 110
+    
+def test_calcelate_tax_discount_round():
+    assert calculate_tax(100.0, 11.1111) == 111.11
 
 @pytest.mark.parametrize('price, tax_rate, expection, message', [
     (-100, 10, ValueError, "Неверная цена"),
